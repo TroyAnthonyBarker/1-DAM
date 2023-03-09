010a40034caf575102f3f085a4dd62540dcc5235
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-03-09 13:53:33 +0100

    Cookies actualizado

--- Lenguajes de marcas y sistemas de gestión de información/Codigo/JS/Cookies/CookiesEpicGames.js
+++ Lenguajes de marcas y sistemas de gestión de información/Codigo/JS/Cookies/CookiesEpicGames.js
@@ -1,11 +1,28 @@
 let acceptCookies = confirm("Ver cookies");
 var contenedor = document.getElementById('containerCookies');
 
-alert(acceptCookies);
+if (!localStorage.getItem('cookies')){
+    alert(acceptCookies);
 
-if (acceptCookies){
-    contenedor.style.display = 'visible'
-} else {
-    window.close()
+    if (acceptCookies){
+        contenedor.style.visibility = 'visible';
+    } else {
+        closeWindow();
+    }
 }
 
+function closeWindow(){
+    window.close();
+}
+
+function acceptNecessary(){
+    contenedor.style.visibility = 'hidden';
+    localStorage.setItem("Hola", "algo.txt")
+}
+
+function showCookies(){
+    var limitedCookiesInfo = document.getElementById('limitedInformationOnCookies');
+    limitedCookiesInfo.style.visibility = 'hidden';
+    var unlimitedCookiesInfo = document.getElementById('unlimitedInformationOnCookies');
+    unlimitedCookiesInfo.style.visibility = 'visible';
+}
