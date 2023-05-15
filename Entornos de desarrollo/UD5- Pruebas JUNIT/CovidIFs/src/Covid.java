2a9371d9545fc6ef41758764d2db29bee302fa2b
TroyAnthonyBarker <trobar@alu.edu.gva.es>
2023-05-15 10:18:30 +0200

    Entornos 	TASCA REFACTOR– GIT 6 		Refactorizado con ayuda de Chat GPT

--- Entornos de desarrollo/UD5- Pruebas JUNIT/CovidIFs/src/Covid.java
+++ Entornos de desarrollo/UD5- Pruebas JUNIT/CovidIFs/src/Covid.java
@@ -2,25 +2,20 @@ public class Covid {
     public static String covid(float bodyTemperature, boolean difficultyBreathing,
                                boolean diabetes, boolean cancer, boolean isPregnant,
                                boolean isOver60yearsold, boolean hepatic,
-                               boolean kidnevDisease, boolean respiratoryDisease, String provincia
-    ){
-        boolean bodyTempMoreOrEqualThan38 = bodyTemperature >= 38;
-        if (bodyTempMoreOrEqualThan38){
-            if (difficultyBreathing
-                    || diabetes
-                    || cancer
-                    || isPregnant
-                    || isOver60yearsold
-                    || hepatic
-                    || kidnevDisease
-                    || respiratoryDisease
-            ){
-                return "/diagnostico/"+provincia;
-            }
-        }
-        else {
+                               boolean kidnevDisease, boolean respiratoryDisease,
+                               String provincia) {
+
+        boolean hasFever = bodyTemperature >= 38;
+        boolean meetsCriteria = difficultyBreathing || diabetes || cancer || isPregnant ||
+                isOver60yearsold || hepatic || kidnevDisease || respiratoryDisease;
+
+        if (hasFever && meetsCriteria) {
+            return "/diagnostico/" + provincia;
+        } else if (hasFever) {
             return "/cuarentena/";
-        }
+        } else {
             return "/diagnostico_bueno/";
+        }
     }
+
 }
